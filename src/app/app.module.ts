210c6e8945bdbf42ac4b26baf039c445d770d85c
Pornthida-P <pornthida@convergence.co.th>
2023-10-11 14:33:35 +0700

    Add satisfaction score and env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent
-  ],
+  declarations: [AppComponent, DashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import {
+  faMeh,
+  faSadCry,
+  faSadTear,
+  faSmile,
+  faSmileWink,
+} from '@fortawesome/free-regular-svg-icons';
+
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  faUsers = faUsers;
+  smiley = [
+    {
+      name: 'Excellent',
+      percent: 47,
+      bgColor: '#1791f4',
+      icon: faSmileWink,
+    },
+    {
+      name: 'Good',
+      percent: 46,
+      bgColor: '#00bdb4',
+      icon: faSmile,
+    },
+    {
+      name: 'Average',
+      percent: 5,
+      bgColor: '#ffb443',
+      icon: faMeh,
+    },
+    {
+      name: 'Poor',
+      percent: 0,
+      bgColor: 'tomato',
+      icon: faSadTear,
+    },
+    {
+      name: 'Very Poor',
+      percent: 0,
+      bgColor: 'crimson',
+      icon: faSadCry,
+    },
+  ];
 }
